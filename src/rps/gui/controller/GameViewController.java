927ef82441bea9c967542da4ace54c1fae08fc28
Filip <91468087+Neshi24@users.noreply.github.com>
2022-03-01 15:06:47 +0100

    gui part 1

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -1,7 +1,14 @@
 package rps.gui.controller;
 
 // Java imports
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -10,12 +17,28 @@ import java.util.ResourceBundle;
  * @author smsj
  */
 public class GameViewController implements Initializable {
-
+    @FXML
+    private Button pressRock;
+    @FXML
+    private Button pressPaper;
+    @FXML
+    private Button pressScissor;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    }
+    
+
+
+
+    public void pressRock(ActionEvent actionEvent) {
+    }
+
+    public void pressPaper(ActionEvent actionEvent) {
+    }
+
+    public void pressScissor(ActionEvent actionEvent) {
     }
 }
